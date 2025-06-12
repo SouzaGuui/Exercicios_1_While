84aaa863096d8a5373ab322b74476d9538d5a0d3
Guilherme de Souza <dsouzaleite49.gmail.com>
2025-06-12 11:08:39 -0300

    commit

---
+++ desafio_1.js/desafio_1.js
@@ -0,0 +1,16 @@
+1
+
+
+let nome
+
+nome = prompt("Digite seu nome: ")
+
+while(nome == ""){
+
+    console.log("Nome invalído!")
+
+    nome = prompt("Digite seu nome: ")
+
+}
+
+console.log ("Bem vindo")
