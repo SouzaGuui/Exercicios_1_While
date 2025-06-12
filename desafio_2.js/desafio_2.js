d1dfa833dd1ab5b9bd71810ff3669049bd7b4280
Guilherme de Souza <dsouzaleite49.gmail.com>
2025-06-12 11:13:00 -0300

    commit

---
+++ desafio_2.js/desafio_2.js
@@ -0,0 +1,17 @@
+2
+
+
+let senha
+senha = prompt("Digite sua senha: ")
+while(senha !== "12345"){
+    console.log("Senha invalída!")
+    nome = prompt("Digite sua senha: ")
+}
+console.log ("Bem vindo")
+let senha
+senha = prompt("Digite seu senha: ")
+while(senha !== "12345"){
+    console.log("Senha invalída!\n Digite Novamente!\n")
+    senha = prompt("Digite seu nome novamente: ")
+}
+console.log("Bem Vindo")
